c001eb2705b67ea057954cf7a2fcdc2416b8dccb
FirstName LastName <albasaenz89@gmail.com>
2021-10-11 09:00:28 +0200

    Añado clase conexion BD

---
+++ GestionHotelesDam/src/ConexionBD/Conexion.java
@@ -0,0 +1,33 @@
+package ConexionBD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+	
+	private static final String CONTROLADOR = "com.mysql.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://localhost:3306/gestionhoteles";
+	private static final String USUARIO = "root";
+	private static final String CLAVE = "";
+	
+	public static Connection conectar() {
+		
+		Connection con = null;
+		try {
+			Class.forName(CONTROLADOR);
+			con = DriverManager.getConnection(URL, USUARIO, CLAVE);
+			System.out.println("CONNECT");
+			
+		} catch (ClassNotFoundException e) {
+				System.out.println("Error al cargar el controlador");
+				e.printStackTrace();
+
+		} catch (SQLException e) {
+				System.out.println("Error con la conexión a la  BBDD");
+				e.printStackTrace();
+			}
+		
+		return con;
+	}
+}
